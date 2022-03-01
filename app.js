0474fa0c255ae2d400ca9a814ae650926c482524
liezsm <liez.dev@gmail.com>
2022-03-01 20:42:42 +0800

    Added the functionality which to generate advice

---
+++ app.js
@@ -0,0 +1,53 @@
+const adviceBtn = document.querySelector('.advice-btn')
+
+
+adviceBtn.addEventListener('click', () => {
+
+    // exp it cached the advice so when you click, it doesnt give a new advice so I'll use the one with ID instead
+//   ( async () =>{
+        
+
+//     try {
+
+//         const url ='https://api.adviceslip.com/advice';
+//         const resp = await fetch(url)
+//         const data = await resp.json()
+      
+//         const { id, advice} = data.slip;
+
+//        console.log(data.slip)
+//        console.log(id, advice)
+
+//         document.getElementById('advice_number').textContent =id;
+//         document.querySelector('.advice-content').textContent = `\"${advice}\"`;
+//     }catch(err){
+//         console.log(err)
+//     }
+    
+// })()
+
+
+(  async() =>{
+    
+    try {
+        
+        const random = Math.floor( Math.random() * 100)
+        const url =`https://api.adviceslip.com/advice/${random}`;
+        const resp = await fetch(url)
+        const data = await resp.json()
+      
+        const { id, advice} = data.slip;
+
+    //    console.log(data)
+       console.log(id, advice)
+
+        document.getElementById('advice_number').textContent =id;
+        document.querySelector('.advice-content').textContent = `\"${advice}\"`;
+    }catch(err){
+        console.log(err)
+    }
+})();
+
+
+})
+
